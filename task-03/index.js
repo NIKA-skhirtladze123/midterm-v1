aec727be01f07952c22844a69302ecc09cb90e9e
NIKA-skhirtladze123 <n_skhirtladze@cu.edu.ge>
2025-04-11 17:35:14 +0400

    Add files via upload

--- task-03/index.js
+++ task-03/index.js
@@ -27,3 +27,52 @@ const users = [
     avatar: "https://fakeimg.pl/150x150",
   },
 ];
+
+const list = document.querySelector('.user-list');
+function createCard(name, email, avatar, id){
+  const div = document.createElement('div');
+  div.setAttribute('calss', 'card');
+  div.innerHTML = `
+     <div class="card-header">
+            <div class="user-info">
+              <div class="avatar">
+                <img src="${avatar}" alt="John Doe" />
+              </div>
+              <div class="user-details">
+                <h3>${name}</h3>
+                <p class="user-email">${email}</p>
+              </div>
+            </div>
+          </div>
+          <div class="card-content">
+            <p class="user-id">User ID: ${id}</p>
+          </div>
+  `
+  list.appendChild(div);
+}
+
+function deleteCards(div){
+    div.remove();
+}
+
+function displayCards(){
+  users.map((user) => {
+    createCard(user.name, user.email, user.avatar, user.id);
+  })
+}
+
+const BUTTON = document.querySelector('#toggleButton');
+displayCards();
+let check = false;
+BUTTON.addEventListener('click', () => {
+  if(check == false){
+    BUTTON.innerText = 'Show Users';
+    list.style.display = "none";
+    check = true;
+  }else{
+    list.style.display = "grid";
+    BUTTON.innerText = 'Hide Users';
+    check = false;
+  }
+})
+
